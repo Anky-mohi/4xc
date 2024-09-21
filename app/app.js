bd565f583ea91fb841e9a3cd13300a4be57243d7
rinkexz <rinkexzpro@gmail.com>
2024-09-21 10:02:58 +0530

    folder setup

--- app/app.js
+++ app/app.js
@@ -3,10 +3,10 @@ const app = express();
 const {
     globalErrHandler,
     notFoundErr,
-} = require("../middlwares/globalErrHandler");
-const multipart = require('../middlwares/multer.middlware');
-const { urlEncoded, jsonEncoded } = require('../middlwares/bodyParser.middlware');
-const cors = require('../middlwares/cors.middlware');
+} = require("../server/middlwares/globalErrHandler");
+const multipart = require('../server/middlwares/multer.middlware');
+const { urlEncoded, jsonEncoded } = require('../server/middlwares/bodyParser.middlware');
+const cors = require('../server/middlwares/cors.middlware');
 const path = require("path");
 
 // here all routes file
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const http = require("http");
 require("./config/dbConnect");
-const app = require("./app/app");
+const app = require("../app/app");
 const PORT = process.env.PORT || 3800;
 
 const server = http.createServer(app);
