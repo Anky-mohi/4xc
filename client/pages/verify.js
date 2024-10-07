bfedd78c2eced39e3cef7de658bcce56edb3e445
rinkexz <rinkexzpro@gmail.com>
2024-10-07 08:45:11 +0530

    change api

--- client/pages/verify.js
+++ client/pages/verify.js
@@ -29,7 +29,7 @@ export default function Verify() {
         setSuccess("Verification successful! Redirecting...");
         // Optionally redirect to dashboard or another page after a timeout
         setTimeout(() => {
-          router.push("https://oauth.deriv.com/oauth2/authorize?app_id=64508");
+          router.push("https://oauth.deriv.com/oauth2/authorize?app_id=64531");
         }, 2000);
       } else {
         setError(response.data.message || "Verification failed. Please try again.");
